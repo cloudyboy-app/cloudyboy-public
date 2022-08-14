09ef8ba1c7b684f3f4e89e57205227f596376a32
Maciej Pędzich <dnalop.me@gmail.com>
2022-08-14 14:26:17 +0200

    Use Supabase module in Nuxt app

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,7 +4,7 @@ import eslintPlugin from 'vite-plugin-eslint';
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
   css: ['@fortawesome/fontawesome-free/css/all.min.css'],
-  modules: ['@nuxtjs/tailwindcss', '@vueuse/nuxt'],
+  modules: ['@nuxtjs/supabase', '@nuxtjs/tailwindcss', '@vueuse/nuxt'],
   vite: {
     plugins: [eslintPlugin()]
   }
