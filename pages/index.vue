f5d7c45a046477fdda52853e1a8bd4685b42e407
Maciej Pędzich <dnalop.me@gmail.com>
2022-08-16 20:31:59 +0200

    Implement non-root redirect handling

--- pages/index.vue
+++ pages/index.vue
@@ -1,66 +1,34 @@
 <script setup lang="ts">
-import { Game } from '~~/models/game';
+definePageMeta({ layout: 'hero' });
 
-const route = useRoute();
-const supabase = useSupabaseClient();
+const router = useRouter();
+const user = useSupabaseUser();
+const redirectCookie = useCookie<string | null>('redirect');
 
-const {
-  pending,
-  error,
-  data: games,
-  refresh
-} = await useAsyncData('home-page-games', async () => {
-  const { data: recentlyPlayedGames } = await supabase
-    .from<Game>('games')
-    .select()
-    .not('date_last_played', 'is', null)
-    .throwOnError();
+watch(user, async (newUserData) => {
+  const redirectPath = redirectCookie.value;
 
-  if (recentlyPlayedGames.length > 0) return recentlyPlayedGames;
-
-  const { data: allGames } = await supabase
-    .from<Game>('games')
-    .select('*')
-    .throwOnError();
-
-  return allGames;
+  if (newUserData && redirectPath) {
+    redirectCookie.value = null;
+    await router.replace(redirectPath);
+  }
 });
-
-const subtitleFragment = computed(() =>
-  games.value?.every((game) => game.date_last_played === null)
-    ? 'newly added'
-    : 'recently played'
-);
-
-onMounted(
-  () =>
-    (route.meta.layout =
-      error.value || games.value?.length === 0 ? 'hero' : 'default')
-);
 </script>
 
 <template>
-  <div class="lg:w-5/6 md:w-4/5 w-3/5">
-    <i v-if="pending" class="fa-solid fa-spinner fa-spin text-8xl"></i>
-    <div v-else-if="error" class="alert alert-error shadow-lg flex-col">
-      <div class="text-base">Something went wrong while fetching games</div>
-      <button class="btn btn-sm" @click="refresh()">Try again</button>
-    </div>
+  <div class="w-11/12">
+    <i v-if="redirectCookie" class="fa-solid fa-spinner fa-spin text-8xl"></i>
     <template v-else>
-      <template v-if="games.length === 0">
-        <h1 class="text-5xl font-bold">Welcome to CloudyBoy!</h1>
-        <p class="text-xl py-5">
-          It looks like you haven't uploaded any games yet...
-        </p>
-        <NuxtLink to="/games/upload" class="btn btn-primary text-sm">
-          <i class="fa-solid fa-cloud-arrow-up mr-3"></i>
-          Upload a game
-        </NuxtLink>
-      </template>
-      <template v-else>
-        <h1 class="text-4xl font-bold mb-4">Welcome back!</h1>
-        <p class="text-xl mb-12">Here are your {{ subtitleFragment }} games</p>
-        <div
+      <h1 class="md:text-5xl text-4xl font-bold">Welcome to CloudyBoy!</h1>
+      <p class="md:text-xl text-lg my-6">
+        Upload and play homebrew Game Boy games directly from the browser
+      </p>
+      <NuxtLink to="/games/upload" class="btn btn-primary text-sm">
+        <i class="fa-solid fa-search mr-3"></i>
+        Browse games
+      </NuxtLink>
+    </template>
+    <!-- <div
           class="grid gap-11 grid-cols-1 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 z-10"
         >
           <NuxtLink
@@ -84,8 +52,6 @@ onMounted(
               </div>
             </div>
           </NuxtLink>
-        </div>
-      </template>
-    </template>
+        </div> -->
   </div>
 </template>
