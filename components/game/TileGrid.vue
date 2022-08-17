f0fb97b47ca69ec95236216084de37de59dd75d1
Maciej Pędzich <dnalop.me@gmail.com>
2022-08-17 10:37:16 +0200

    Extract commented out tile grid to a component

---
+++ components/game/TileGrid.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+import { Game } from '~~/models/game';
+
+const props = defineProps<{ games: Game[] }>();
+const { games } = toRefs(props);
+</script>
+
+<template>
+  <div
+    class="grid gap-11 grid-cols-1 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 z-10"
+  >
+    <NuxtLink v-for="game of games" :key="game.id" :to="`/games/${game.id}`">
+      <div
+        class="card card-compact w-full bg-base-100 shadow-xl transition-all ease-in-out duration-300 hover:shadow-2xl hover:scale-110 active:shadow-md active:scale-90 z-10"
+      >
+        <figure class="max-w-lg h-auto p-5 pb-2">
+          <img
+            class="rounded-md shadow-2xl"
+            :src="(game.cover as string)"
+            :alt="`Cover image of ${game.title}`"
+          />
+        </figure>
+        <div class="card-body items-center text-center">
+          <h2 class="card-title">{{ game.title }}</h2>
+          <p class="truncate">
+            Uploaded by
+            <strong>{{ game.uploader.raw_user_meta_data.user_name }}</strong>
+          </p>
+        </div>
+      </div>
+    </NuxtLink>
+  </div>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -24,34 +24,8 @@ watch(user, async (newUserData) => {
         Upload and play homebrew Game Boy games directly from the browser
       </p>
       <NuxtLink to="/games/upload" class="btn btn-primary text-sm">
-        <i class="fa-solid fa-search mr-3"></i>
         Browse games
       </NuxtLink>
     </template>
-    <!-- <div
-          class="grid gap-11 grid-cols-1 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 z-10"
-        >
-          <NuxtLink
-            v-for="game of games"
-            :key="game.id"
-            :to="`/games/${game.id}`"
-          >
-            <div
-              class="card card-compact w-full bg-base-100 shadow-xl transition-all ease-in-out duration-300 hover:shadow-2xl hover:scale-110 active:shadow-md active:scale-90 z-10"
-            >
-              <figure class="max-w-lg h-auto p-5 pb-2">
-                <img
-                  class="rounded-md shadow-2xl"
-                  :src="(game.cover as string)"
-                  :alt="`Cover image of ${game.title}`"
-                />
-              </figure>
-              <div class="card-body items-center text-center">
-                <h2 class="card-title">{{ game.title }}</h2>
-                <p class="truncate">By <strong>AUTHOR HERE</strong></p>
-              </div>
-            </div>
-          </NuxtLink>
-        </div> -->
   </div>
 </template>
